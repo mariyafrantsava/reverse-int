73c8b62812fe56c7658051493a6fdfcf59f52881
mariyafrantsava <mariya.frantsava7@gmail.com>
2020-10-20 15:36:37 +0300

    feat: make and finish a task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse(n) {
-
+    n = String(n).replace('-', '');
+    let lastChar = n.length - 1;
+    if (n[lastChar] === '0') {
+        n = n.slice(0, lastChar);
+    }
+    n = n.split('').reverse().join('');
+    return n;
 }
